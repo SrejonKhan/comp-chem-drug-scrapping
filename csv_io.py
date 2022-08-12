fe9a60acaf794bd84a4746c6515a088ec5f5d24c
AfrinRai <islamrai13@gmail.com>
2022-08-13 00:34:33 +0600

    implement swissadme header writing

--- csv_io.py
+++ csv_io.py
@@ -5,9 +5,24 @@ def write_to_csv(filename,row):
         writer = csv.writer(file)
         writer.writerow(row)
 
-# rai 
-def write_swissadme_headers():
-    pass
+def write_swissadme_headers(filename):
+    swissadme_header_row = ["Name of drugs", "Lipsinki (04)", "Violation", "Ghose (04)", "Violation", 
+        "Veber (02)", "Violation", "Egan", "Violation", "Muegge", "Violation"]
+    swissadme_sub_header_row = ["", 
+        # Lipinski
+        "Molecular Weight", "MLOGP", "No of H Donor", "No of H acceptor", "", 
+        # Ghose
+        "Molecular Weight", "MLOGP", "Molar Refractivity", "Total Atoms", "", 
+        # Veber
+        "Rotatable Bond", "TPSA", "", 
+        # Egan
+        "MLOGP", "TPSA", "", 
+        # Muegge
+        "Molecular Weight", "TPSA", "Num. of RINGS", "Num. of CARBON", "Num. HETEROATOM", 
+        "Num. Rotatable bonds", "H-bond acceptors", "H-bond donors"]
+    
+    write_to_csv(filename, swissadme_header_row)
+    write_to_csv(filename, swissadme_sub_header_row)
 
 # maskura
 def write_pkcsm_headers():
